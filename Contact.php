b9bf400d3d02e00de2cbd91764396d42a4d39d54
101608308 <62002059+101608308@users.noreply.github.com>
2020-05-25 20:38:15 +1000

    Add files via upload

--- Contact.php
+++ Contact.php
@@ -16,7 +16,7 @@
 <body>  
 
 <header>
-  <img id="product_logo" src="logo.webp" alt="pdlogo" />
+  <img id="product_logo" src="images/logo.png" alt="pdlogo" />
   <h1 class="product_name"> One Stop Movies </h1>
 </header>
 
@@ -30,27 +30,24 @@
 <?php
 
 
-$userdetail = $userdetail_error = "";
+$name = $name_error = "";
 $email = $email_error =  "";
 $number = $number_error = "";
-$messagearea = $messagearea_error = "";
-$complaintstype_error ="";
-$complaintstype = $_POST["complaintstype"];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
-  if (empty($_POST["userdetail"])) 
+  if (empty($_POST["name"])) 
   {
-    $userdetail_error = "Name is required";
+    $name_error = "Name is required";
   } 
 
   else 
   {
-    $name = test_input($_POST["userdetail"]);
+    $name = test_input($_POST["name"]);
     
-    if (!preg_match("/^[a-zA-Z ]*$/",$userdetail)) 
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) 
     {
-      $userdetail_error = "Please use letters only!";
+      $name_error = "Please use letters only!";
     }
   }
   
@@ -71,7 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     
   if (empty($_POST["number"])) 
   {
-    $number_error = "Number is required";
+    $number = "";
   } 
 
   else 
@@ -83,18 +80,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
       $number_error = "Invalid Number";
     }
   }
-
-  if (empty($_POST["messagearea"])) 
-  {
-    $messagearea_error = "Complaint is required";
-  }
-
-  if($complaintstype =="none")
-  {
-    $complaintstype_error ="Complaint type is required";
-  }
-
-  
 }
 
 function test_input($data) {
@@ -109,9 +94,9 @@ function test_input($data) {
 <p><span class="error">* Important fields</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-  Name: <span class="error">*</span> <input type="text" name="userdetail" placeholder="peter" value="<?php echo $userdetail;?>">
-  <span class="error"><?php echo $userdetail_error;?></span>
-  <br><br>  
+  Name: <span class="error">*</span> <input type="text" name="name" placeholder="John Doe" value="<?php echo $name;?> ">
+  <span class="error"><?php echo $name_error;?></span>
+  <br><br>
 
   E-mail: <span class="error">*</span> <input type="text" name="email" placeholder="someone@example.com" value="<?php echo $email;?>">
   <span class="error"><?php echo $email_error;?></span>
@@ -121,25 +106,9 @@ function test_input($data) {
   <span class="error"><?php echo $number_error;?></span>
   <br><br>
 
-Select the type of Complaint:
-  <select name="complaintstype" id="complaintstype">  
-    <option value="none" selected="selected">Select an option</option>
-    <option value="Service">Service</option>
-    <option value="Price">Price</option>
-    <option value="Refund">Refund</option>
-  </select>
-  <span class="error"><?php echo$complaintstype_error;?></span>
-  <br><br>  
-
-   
-  Please Enter your complaint here.<textarea id="messagearea" rows="5" cols="50" value="<?php echo $messagearea;?>"></textarea>  
-  <span class="error"><?php echo $messagearea_error;?></span>
-  <br><br>
   <input type="submit" name="submit" value="Submit">  
-  <button class="button" type="reset" id="cancel" onclick="location.href='product.php';"" value="Go to home page">Cancel Complaint</button>
 </form>
 </div>
-
 
 
 </body>
